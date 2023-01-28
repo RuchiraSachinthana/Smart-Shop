d6703e46c6d5f80794e43668f52cb6bb9f9013ee
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2023-01-29 00:57:42 +0530

    created arrays for the project

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,13 @@
 public class AppInitializer {
+
+    //   ==================== Database Area ===================== (for access all around the project)
+
+    static String[][] users = new String[3][2];
+    static String[][] customers = new String[100][4];
+    static String[][] items = new String[100][4];
+    static String[][] orders = new String[100][5];
+
     public static void main(String[] args) {
-        
+
     }
 }
