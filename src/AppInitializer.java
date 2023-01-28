10e100a48bbc55c68588e2a7893ca5093718d272
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2023-01-29 01:00:05 +0530

    Created main method with switch case and while loop and other sub methods

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class AppInitializer {
 
     //   ==================== Database Area ===================== (for access all around the project)
@@ -8,6 +10,66 @@ public class AppInitializer {
     static String[][] orders = new String[100][5];
 
     public static void main(String[] args) {
+
+//        ==================== Test Data =====================
+
+        items[0][0] = "001";
+        items[0][1] = "Desc 01";
+        items[0][2] = "15";
+        items[0][3] = "250";
+
+//        ==================== Test Data =====================
+
+//        Programme initialization process
+        printUi("Welcome To the Application");
+
+//        create Scanner object
+        Scanner scanner = new Scanner(System.in);
+        boolean exitState = false;
+//        create array for login
+        String[] initializePageQuestions = {"1) Do you want to login?", "2) Do you want to register?", "3) Do you want to exit?"};
+
+        while (!exitState) {
+// create for loop to print out the questions
+            for (String question : initializePageQuestions) {
+                System.out.println(question);
+            }
+
+
+//        getting user input
+            int userInput = scanner.nextInt();
+//        create switch statement to handle user input
+            switch (userInput) {
+                case 1:
+//         Just for testing process
+                    users[0][0] = "ruchira@test.com";
+                    users[0][1] = "test123";
+                    if (login()) {
+                        printUi("Dashboard");
+                        openDashboard();
+
+                    }
+                    printUi("Application");
+                    endUI();
+                    break;
+                case 2:
+                    if (register()) {
+                        printUi("Dashboard");
+                        openDashboard();
+                    }
+                    printUi("Application");
+                    endUI();
+                    break;
+                case 3:
+                    System.out.println("You have selected to exit" + "Good Bye!");
+                    endUI();
+                    return;
+                default:
+                    System.out.println("Invalid input.." + "\uD83D\uDE14");
+                    endUI();
+                    return;
+            }
+        }
 
     }
 }
